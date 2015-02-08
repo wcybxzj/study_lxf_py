44de1b3e86a24a8d4d12840a53882b3e0c0918f6
wcybxzj <virgo86ybx@gmail.com>
2015-02-09 01:25:46 +0800

    函数参数

--- 5.function/5.3.function_arg.py
+++ 5.function/5.3.function_arg.py
@@ -9,23 +9,60 @@ class func_arg(object):
 
     #默认参数必须指向不变对象
     def add_end_ok(self, L=None):
-        if L is N   one:
+        if L is None:
             L = []
         L.append('END')
         return L
 
+    def change_arg(self, *numbers):
+        sum = 0
+        for n in numbers:
+            sum = sum + n *n
+        return sum
+
+    def key_word_arg(self, name, age, **kw):
+        print 'name', name, 'age', age, 'kw', kw
+
+    def assembly_arg(self, name, age, school='beida', *args, **kw):
+        print 'name:', name, 'age:', age, 'school', school, 'args:', args, 'kw:',kw
+
+
     def run(self):
+        #part 1 默认参数
         print self.add_end_bug([1, 2, 3])
         print self.add_end_bug(['x', 'y', 'z'])
 
-        print self.add_end_bug()
-        print self.add_end_bug()
-        print self.add_end_bug()
+        for i in range(3):
+            print self.add_end_bug()
 
-        #wrong
-        print self.add_end_ok()
-        print self.add_end_ok()
-        print self.add_end_ok()
+        for i in range(3):
+            print self.add_end_ok()
+
+        #part 2 可变参数
+        #可以传入0->N 个参数
+        print self.change_arg(2, 3, 4)
+        print self.change_arg()
+        nums = [1, 2, 3]
+        print self.change_arg(*nums)
+
+        #part 3 关键字参数
+        self.key_word_arg('ybx', 22)
+        self.key_word_arg('ybx', 22, city='Beijing')
+        self.key_word_arg('ybx', 22, gender='male', job='doctor')
+        key_word = {'hobby':'reading', 'friend':'jsk'}
+        self.key_word_arg('ybx', 22, **key_word)
+
+        #part 4组合参数
+        self.assembly_arg('ybx', 18)
+        self.assembly_arg('ybx', 18, '清华', 'aa', 'bb')
+        self.assembly_arg('ybx', 18, '清华', 'aa', 'bb', hobby='reading book')
+
+        args = (1, 2, 3, 4)
+        kw = {'x':99}
+        self.assembly_arg(*args, **kw)
+
+        args = [11, 22, 33, 44]
+        self.assembly_arg(*args, **kw)
 
 obj = func_arg()
 obj.run()
