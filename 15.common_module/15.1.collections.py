cd97c4159bfb6f3e535af53667fd7abe20e82dd6
ybx <87212426@qq.com>
2015-02-18 20:02:28 +0800

    collections

--- 15.common_module/15.1.collections.py
+++ 15.common_module/15.1.collections.py
@@ -1,17 +1,44 @@
 __author__ = 'yangbingxi'
 
 
-from collections import namedtuple
+from collections import namedtuple, deque, defaultdict, OrderedDict
 
 class mycollections(object):
+
     def namedtuple(self):
         Point = namedtuple('Point', ['x', 'y'])
         p = Point(1, 2)
         print p.x
         print p.y
+        print isinstance(p, Point)
+        print isinstance(p, tuple)
+
+    def deque(self):
+        mylsit = ['a', 'b', 'c']
+        q = deque(mylsit)
+        q.append('x')
+        q.appendleft('y')
+        print q
+
+    def defaultdict(self):
+        dd = defaultdict(lambda: 'N/A')
+        print dd['not_exists_key']
+
+    def OrderedDict(self):
+        mylist = [('a', 1), ('b', 2), ('c', 3)]
+        print dict(mylist)
+        print OrderedDict(mylist)
+
+
 
     def run(self):
         self.namedtuple()
+        self.deque()
+        self.defaultdict()
+        self.OrderedDict()
+
+
+
 
 
 obj = mycollections()
